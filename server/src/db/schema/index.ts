330adcfd23f21008a6fd76f962ccd800aa604576
KrishParekh <krishparekh303@gmail.com>
2026-01-02 18:19:26 +0530

    feat: added schema types for queries

---
+++ server/src/db/schema/index.ts
@@ -0,0 +1,6 @@
+export * from "./auth";
+export * from "./companies";
+export * from "./problems";
+export * from "./problem_companies";
+export * from "./submissions";
+export * from "./test_cases";
